c856f0437190fc91ef0efd293bebdb0c88cc17c3
linchang77 <2250420@tongji.edu.cn>
2023-12-10 20:48:50 +0800

    add UI files

---
+++ Heroes.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef HEROES_H
+#define HEROES_H
+
+#include "cocos2d.h"
+
+//Ð¡Ð¡Ó¢ÐÛ
+class LittleHero : public cocos2d::Sprite
+{
+public:
+
+private:
+
+};
+
+//Ó¢ÐÛ
+class Hero : public cocos2d::Sprite
+{
+public:
+
+private:
+
+};
+
+#endif // !HEROES_H
