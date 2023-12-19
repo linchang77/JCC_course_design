5f6350bf48b58b794fadab4f02afdb3dc0e66da2
Xuejinye <3323048008@qq,com>
2023-12-19 19:27:22 +0800

    add 01

--- classes/Heroes.h
+++ classes/Heroes.h
@@ -10,20 +10,17 @@
 #define MAX_COST 5		//英雄最大价格
 
 //小小英雄
-class LittleHero : public cocos2d::Node
+class LittleHero : public cocos2d::Sprite
 {
 public:
 	//单例？
 	static LittleHero* getInstance();
 
 	//返回等级
-	int getGrade() const;
-
-	CREATE_FUNC(LittleHero);
+	int getGrade();
 
 private:
-	cocos2d::Sprite* body;		//精灵指针
-	int grade;					//等级
+	int grade;			//等级
 };
 
 struct HeroImages;
@@ -32,29 +29,51 @@ struct HeroImages;
 class Hero : public cocos2d::Node
 {
 public:
-	HeroImages getImages() const;		//获取英雄图像信息
-	int getCost() const;				//获取价格
-	//其它东西请棋子设计者自行实现
-
+	virtual bool init();    //初始化英雄
+	void setSpace(cocos2d::Layer* space)
+	{
+		hero_space = space;
+	};
+	void Appear_in_Preparation();    //渲染并出现在备战席
+	bool initHp();    //初始化血条
+	void updateHp(float delta);    //血条刷新
+	bool initMp();    //初始化蓝条
+	void updateMp(float delta);    //蓝条刷新
+	void Death();    //死亡淡出动画
+	void Move();    //棋子移动
+	void Attack();    //棋子攻击
+	HeroImages getImages();							//获取英雄图像信息
+	int getCost();									//获取价格
+	static Hero* copy(Hero* templt);    //
 	CREATE_FUNC(Hero);
 
-protected:
-	cocos2d::Sprite* body;				//精灵指针
-	std::string imageOnField;			//战场形象（多个，暂时只开一个，后面可能要改成Vector）
+private:
+	std::string imageOnField;			//战场形象（多个，暂时只开一个，后面可能要改成样例）
 	std::string imageInStoreNormal;		//商店形象（正常）
 	std::string imageInStoreSelected;	//商店形象（被选中）
 	int cost;							//价格
 	std::string name;					//名称
+	HeroPosition position;    //英雄战斗位置坐标
+	cocos2d::Layer* hero_space;    //棋子所在的空间层
+	cocos2d::Sprite* body;    //棋子
+	cocos2d::Sprite* hp_bar;    //血条框
+	cocos2d::Sprite* hp;    //血条
+	float maxhp;    //最大血量
+	float realhp;    //实时血量
+	float range;    //攻击距离
+	float frequency;    //攻击频率
+	cocos2d::Sprite* mp_bar;    //蓝条框
+	cocos2d::Sprite* mp;    //蓝条
 };
 
-//（样例）英雄子类
-class Example : public Hero
+struct HeroPosition
+	//英雄战斗位置坐标
 {
-public:
-	virtual bool init();
-
-	CREATE_FUNC(Example);
+	int x;    //横坐标
+	int y;    //纵坐标
 };
+
+
 
 //英雄图像信息
 struct HeroImages
@@ -63,5 +82,13 @@ struct HeroImages
 	std::string imageInStoreNormal;		//商店形象（正常）
 	std::string imageInStoreSelected;	//商店形象（被选中）
 };
+
+class Example :public Hero
+	//派生具体英雄例子
+{
+	virtual bool init() override;
+};
+
+
 
 #endif // !HEROES_H
--- classes/Littlehero.cpp
+++
@@ -1,4 +0,0 @@
-#include "Map.h"
-#include "cocos2d.h"
-USING_NS_CC;
-
